c697b30b1825e20daefdfb98a0d163ca66fabe19
dardan09876 <dardan09876@gmail.com>
2025-08-18 16:51:29 -0700

    Player choice

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ let currentPlayer = player1;
 function createPlayer(name, symbol) {
   return {
     name: name,
-    symbol: symbol
+    symbol: symbol,
   };
 }
 
 function printGameBoard() {
+  console.clear();
   console.log(`
    ${gameBoard[0] || "1"} | ${gameBoard[1] || "2"} | ${gameBoard[2] || "3"}
   ---+---+---
@@ -20,20 +21,55 @@ function printGameBoard() {
   `);
 }
 
+function choice(position) {
+  if (typeof position !== "number" || position < 1 || position > 9) {
+    console.log("Pick a # between 1-9");
+    return;
+  }
+
+  if (gameBoard[position - 1] !== "") {
+    console.log("Spot taken, Try again!");
+    return;
+  }
+
+  gameBoard[position - 1] = currentPlayer.symbol;
+
+  if (winGame(currentPlayer.symbol)) {
+    printGameBoard();
+    console.log(`${currentPlayer.name} (${currentPlayer.symbol}) wins!`);
+    return;
+  } else if (gameBoard.every((cell) => cell !== "")) {
+    printGameBoard();
+    console.log("It's a draw!");
+    return;
+  }
+
+  currentPlayer = currentPlayer === player1 ? player2 : player1;
+  printGameBoard();
+  console.log(`${currentPlayer.name}'s turn`);
+}
+
 function winGame(symbol) {
-    const winCombo = [
-        [0,1,2],[3,4,5],[6,7,8],
-        [0,3,6],[1,4,7],[2,5,8],
-        [0,4,8],[2,4,6]
-    ];
-    return winCombo.some(function(pattern) {
-    return pattern.every(function(index) {
+  const winCombo = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  return winCombo.some(function (pattern) {
+    return pattern.every(function (index) {
       return gameBoard[index] === symbol;
     });
   });
 }
 
+printGameBoard();
+console.log(
+  `${currentPlayer.name}, Pick a # on the game board with choice(1-9)!`
+);
 console.log(player1);
 console.log(player2);
-printGameBoard();
-console.log("Tic Tac Toe!");
